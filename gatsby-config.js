0f9d617f9e6b5ca2cdba758bcbab94e63555a8ae
bank8426 <bank_55555@hotmail.com>
2019-06-05 23:47:00 +0700

    Finish

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,6 +1,51 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby Default Starter',
+    title: 'Gatsby Design Code',
+    description:'Complete courses',
+    keywords:'react',
+    footer:{
+      edges : [
+        {
+          node:{
+            title:"Privacy",
+            url:"/privacy"
+          }
+        },
+        {
+          node:{
+            title:"Contact",
+            url:"/contact"
+          }
+        },
+        {
+          node:{
+            title:"About",
+            url:"/about"
+          }
+        },
+        {
+          node:{
+            title:"Courses",
+            url:"/courses"
+          }
+        },
+        {
+          node:{
+            title:"Home",
+            url:"/"
+          }
+        }
+      ]
+    }
   },
-  plugins: ['gatsby-plugin-react-helmet'],
+  plugins: [
+    'gatsby-plugin-react-helmet',
+    /*{
+      resolve: `gatsby-source-contentful`,
+      options:{
+        spaceId:`xgghraa72dwu`,
+        accessToken:`vGJkqn0DLpUN0us3D53S6BozOBy4TGONy_LCgrhU9mg`
+      }
+    }*/
+  ],
 }
